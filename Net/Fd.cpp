786920dbb13976bc2e09866ecd1fcf2959bc5745
ZmnSCPxj jxPCSnmZ <zmnscpxj@protonmail.com>
2020-09-09 07:06:34 +0800

    Net/Fd.cpp: Preserve errno across the destructor.

--- Net/Fd.cpp
+++ Net/Fd.cpp
@@ -1,11 +1,16 @@
 #include"Net/Fd.hpp"
+#include<errno.h>
 #include<unistd.h>
 
 namespace Net {
 
 Fd::~Fd() {
-	if (fd >= 0)
+	if (fd >= 0) {
+		/* Ignore errors.  */
+		auto err = errno;
 		close(fd);
+		errno = err;
+	}
 }
 
 }

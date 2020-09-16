d00b19753c72a5a06d03ab278589f562e013d28e
ZmnSCPxj jxPCSnmZ <zmnscpxj@protonmail.com>
2020-09-16 15:53:12 +0800

    tests/boss/test_waiter_timed.cpp: Stress-test Waiter::timed.
    Does not reveal the bug in `timed` though.

--- tests/boss/test_waiter_timed.cpp
+++ tests/boss/test_waiter_timed.cpp
@@ -3,12 +3,43 @@
 #include"Boss/Shutdown.hpp"
 #include"Ev/Io.hpp"
 #include"Ev/start.hpp"
+#include"Ev/yield.hpp"
 #include"S/Bus.hpp"
 #include<assert.h>
+#include<memory>
 
 int main() {
 	S::Bus bus;
 	Boss::Mod::Waiter waiter(bus);
+
+	auto loop_test = [&]() {
+		auto counter = std::make_shared<std::size_t>(1000);
+		auto ploop = std::make_shared<std::function<Ev::Io<void>()>
+					     >();
+		*ploop = [&waiter, counter, ploop]() {
+			if ((*counter) == 0) {
+				*ploop = nullptr;
+				return Ev::lift();
+			}
+			--(*counter);
+
+			return Ev::lift().then([&waiter]() {
+				/* Yield should complete first.  */
+				auto action = Ev::yield().then([]() {
+					return Ev::lift(true);
+				});
+				return waiter.timed(60, action
+						   ).catching<Boss::Mod::Waiter::TimedOut>([](Boss::Mod::Waiter::TimedOut const& _) {
+					return Ev::lift(false);
+				});
+			}).then([ploop](bool flag) {
+				assert(flag);
+				return (*ploop)();
+			});
+		};
+
+		return (*ploop)();
+	};
 
 	auto code = Ev::lift().then([&]() {
 
@@ -31,6 +62,9 @@ int main() {
 		});
 	}).then([&](bool flag) {
 		assert(!flag);
+
+		return loop_test();
+	}).then([&]() {
 
 		/* Cancel all pending waiters.  */
 		return bus.raise(Boss::Shutdown());

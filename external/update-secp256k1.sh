95affdcb88ae0f6d18357b32064ab6bee86f2bc1
ZmnSCPxj jxPCSnmZ <zmnscpxj@protonmail.com>
2020-09-21 12:26:38 +0800

    external/secp256k1/: Add libsecp256k1 library.

---
+++ external/update-secp256k1.sh
@@ -0,0 +1,42 @@
+#! /bin/sh
+
+REPO="https://github.com/bitcoin-core/secp256k1"
+EXTNAME="secp256k1"
+COMMIT="856a01d"
+
+DIR=`mktemp -d`
+
+if git clone ${REPO} "$DIR"; then
+  :
+else
+  printf 'Failed to clone "%s".\n' "${REPO}" 1>&2
+  exit 1
+fi
+
+ORIGDIR="`pwd`"
+
+cd ${DIR}
+
+if git reset --hard ${COMMIT}; then
+  :
+else
+  printf 'Failed to find commit "%s".\n' "${COMMIT}" 1>&2
+  cd "$ORIGDIR"
+  rm -rf "${DIR}"
+  exit 1
+fi
+
+find '(' -name '.git'  ')' -exec rm -rf '{}' ';'
+
+if patch -p1 < "${ORIGDIR}"/"${EXTNAME}.patch"; then
+  :
+else
+  printf 'Failed to apply patch "%s".\n' "${ORIGDIR}"/"${EXTNAME}.patch" 1>&2
+  cd "$ORIGDIR"
+  rm -rf ${DIR}
+  exit 1
+fi
+
+cd "$ORIGDIR"
+rm -rf "${EXTNAME}" > /dev/null 2>&1
+cp -r "${DIR}" "${EXTNAME}"

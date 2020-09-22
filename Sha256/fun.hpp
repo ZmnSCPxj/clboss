5cb353b480ad9a7bfabae987692c4ad90356332c
ZmnSCPxj jxPCSnmZ <zmnscpxj@protonmail.com>
2020-09-22 11:50:01 +0800

    Sha256/fun.hpp: Add Sha256::fun overload for hashing a hash.

--- Sha256/fun.hpp
+++ Sha256/fun.hpp
@@ -12,6 +12,15 @@ Sha256::Hash fun(void const* p, std::size_t len) {
 	hasher.feed(p, len);
 	return std::move(hasher).finalize();
 }
+/* Convenience version for double-sha256.  */
+inline
+Sha256::Hash fun(Sha256::Hash h) {
+	std::uint8_t buf[32];
+	h.to_buffer(buf);
+	auto hasher = Sha256::Hasher();
+	hasher.feed(buf, sizeof(buf));
+	return std::move(hasher).finalize();
+}
 
 }
 

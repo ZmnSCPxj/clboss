9482a64897ce1f92666033941ad200d301a90612
ZmnSCPxj jxPCSnmZ <zmnscpxj@protonmail.com>
2020-09-25 10:58:02 +0800

    Boltz/Detail/SwapSetupHandler.cpp: Object to handle setting up the swap.

---
+++ Boltz/Detail/SwapSetupHandler.cpp
@@ -0,0 +1,247 @@
+#include"Bitcoin/hash160.hpp"
+#include"Boltz/Connection.hpp"
+#include"Boltz/Detail/SwapSetupHandler.hpp"
+#include"Boltz/Detail/match_lockscript.hpp"
+#include"Boltz/EnvIF.hpp"
+#include"Ev/Io.hpp"
+#include"Jsmn/Object.hpp"
+#include"Json/Out.hpp"
+#include"Secp256k1/SignerIF.hpp"
+#include"Sha256/fun.hpp"
+#include"Sqlite3.hpp"
+#include"Util/Str.hpp"
+#include"Util/make_unique.hpp"
+#include<sstream>
+
+namespace {
+
+/* Thrown to get out and fail.  */
+struct Fail {};
+
+}
+
+namespace Boltz { namespace Detail {
+
+Ev::Io<void> SwapSetupHandler::logd(std::string msg) {
+	return env.logd(prefixlog(std::move(msg)));
+}
+Ev::Io<void> SwapSetupHandler::loge(std::string msg) {
+	return env.loge(prefixlog(std::move(msg)));
+}
+std::string SwapSetupHandler::prefixlog(std::string msg) {
+	auto prefix = std::string("Boltz::Service(\"")
+		    + api_endpoint
+		    + "\"): "
+		    ;
+	if (swapId)
+		prefix += std::string("\"") + *swapId + "\": ";
+	return prefix + msg;
+}
+
+Ev::Io<std::string> SwapSetupHandler::run() {
+	auto self = shared_from_this();
+	return self->core_run().then([self]() {
+		return Ev::lift(std::move(self->invoice));
+	});
+}
+
+Ev::Io<void> SwapSetupHandler::core_run() {
+	return Ev::lift().then([this]() {
+		/* Set up.  */
+		/* PubKey.  */
+		tweakPubKey = signer.get_pubkey_tweak(tweak);
+		/* Preimage.  */
+		std::uint8_t buf[32];
+		preimage.to_buffer(buf);
+		Bitcoin::hash160( preimageHash160, preimageHash
+				, buf, sizeof(buf)
+				);
+
+		auto params = Json::Out()
+			.start_object()
+				.field("type", "reversesubmarine")
+				.field("pairId", "BTC/BTC")
+				.field("orderSide", "buy")
+				.field( "invoiceAmount"
+				      , offchainAmount.to_sat()
+				      )
+				.field( "preimageHash"
+				      , std::string(preimageHash)
+				      )
+				.field( "claimPublicKey"
+				      , std::string(tweakPubKey)
+				      )
+			.end_object()
+			;
+		return conn.api( "/createswap"
+			       , Util::make_unique<Json::Out>(
+					std::move(params)
+				 )
+			       );
+	}).then([this](Jsmn::Object res) {
+		/* Parse result.  */
+		auto tmp_swapId = std::string();
+		auto tmp_s_redeemScript = std::string();
+		auto tmp_invoice = std::string();
+		auto tmp_timeoutBlockheight = std::uint32_t();
+		auto tmp_onchainAmount = std::uint64_t();
+		try {
+			tmp_swapId = (std::string) res["id"];
+			tmp_s_redeemScript = (std::string) res["redeemScript"];
+			tmp_invoice = (std::string) res["invoice"];
+			tmp_timeoutBlockheight = (std::uint32_t) (double) res["timeoutBlockheight"];
+			tmp_onchainAmount = (std::uint64_t) (double) res["onchainAmount"];
+		} catch (Jsmn::TypeError const& e) {
+			auto os = std::ostringstream();
+			os << "Unexpected result from service: "
+			   << res
+			    ;
+			return loge(os.str()).then([]() {
+				throw Fail();
+				return Ev::lift();
+			});
+		}
+		/* Validate redeemScript is hex.   */
+		if (!Util::Str::ishex(tmp_s_redeemScript)) {
+			return loge( std::string("invalid redeemScript: ")
+				   + tmp_s_redeemScript
+				   ).then([]() {
+				throw Fail();
+				return Ev::lift();
+			});
+		}
+		auto tmp_redeemScript = Util::Str::hexread(tmp_s_redeemScript);
+
+		/* Validate redeemScript is correct.  */
+		auto script_hash160 = Ripemd160::Hash();
+		auto script_mypubkey = Secp256k1::PubKey();
+		auto script_locktime = std::uint32_t();
+		auto script_theirpubkey = Secp256k1::PubKey();
+		auto ok = match_lockscript( script_hash160
+					  , script_mypubkey
+					  , script_locktime
+					  , script_theirpubkey
+					  , tmp_redeemScript
+					  );
+		ok = ok
+		  && (script_hash160 == preimageHash160)
+		  && (script_mypubkey == tweakPubKey)
+		  && (script_locktime == tmp_timeoutBlockheight)
+		   ;
+		if (!ok) {
+			return loge( std::string("invalid redeemScript: ")
+				   + tmp_s_redeemScript
+				   ).then([]() {
+				throw Fail();
+				return Ev::lift();
+			});
+		}
+
+		/* FIXME: We should check the latest quotation!  */
+		if ( (Ln::Amount::sat(tmp_onchainAmount) / offchainAmount)
+		   < 0.95
+		   ) {
+			auto os = std::ostringstream();
+			os << "Onchain amount "
+			   << tmp_onchainAmount
+			   << " too low compared to offchain amount "
+			   << offchainAmount
+			    ;
+			return loge(os.str()).then([]() {
+				throw Fail();
+				return Ev::lift();
+			});
+		}
+
+		/* Validation is okay!  Save the data into the object.  */
+		swapId = Util::make_unique<std::string>(std::move(tmp_swapId));
+		redeemScript = std::move(tmp_redeemScript);
+		timeoutBlockheight = std::move(tmp_timeoutBlockheight);
+		onchainAmount = Ln::Amount::sat(tmp_onchainAmount);
+		invoice = std::move(tmp_invoice);
+
+		return Ev::lift();
+	}).then([this]() {
+
+		/* Now save to DB.  */
+		return db.transact();
+	}).then([this](Sqlite3::Tx tx) {
+		/* First check the swapId does not exist yet.  */
+		auto check = tx.query(R"QRY(
+		SELECT swapId FROM "BoltzServiceFactory_rsub"
+		 WHERE apiAccess = :apiAccess
+		   AND swapId = :swapId
+		)QRY")
+			.bind(":apiAccess", api_endpoint)
+			.bind(":swapId", *swapId)
+			.execute()
+			;
+		auto found = false;
+		for (auto& r : check) {
+			(void) r;
+			found = true;
+		}
+		/* Unexpected duplicate key!  */
+		if (found) {
+			return loge("id already exists??").then([]() {
+				throw Fail();
+				return Ev::lift();
+			});
+		}
+		/* Insert it.  */
+		tx.query(R"QRY(
+		INSERT INTO "BoltzServiceFactory_rsub"
+		     ( apiAccess
+		     , tweak
+		     , preimage
+		     , destinationAddress
+		     , swapId
+		     , redeemScript
+		     , timeoutBlockheight
+		     , onchainAmount
+		     , lockedUp
+		     , comment
+		     )
+		VALUES
+		     ( :apiAccess
+		     , :tweak
+		     , :preimage
+		     , :destinationAddress
+		     , :swapId
+		     , :redeemScript
+		     , :timeoutBlockheight
+		     , :onchainAmount
+		     , :lockedUp
+		     , :comment
+		     );
+		)QRY")
+			.bind(":apiAccess", api_endpoint)
+			.bind(":tweak", std::string(tweak))
+			.bind(":preimage", std::string(preimage))
+			.bind(":destinationAddress", destinationAddress)
+			.bind(":swapId", *swapId)
+			.bind(":redeemScript"
+			     , Util::Str::hexdump( &redeemScript[0]
+						 , redeemScript.size()
+						 )
+			     )
+			.bind(":timeoutBlockheight", timeoutBlockheight)
+			.bind(":onchainAmount", onchainAmount.to_sat())
+			.bind(":lockedUp", 0)
+			.bind(":comment", "")
+			.execute()
+			;
+
+		tx.commit();
+
+		return logd(std::string("Swap created: ") + invoice);
+	}).catching<Fail>([this](Fail const& _) {
+		invoice = "";
+		return Ev::lift();
+	}).catching<Boltz::ApiError>([this](Boltz::ApiError const& _){
+		invoice = "";
+		return loge("Failed to access API endpoint.");
+	});
+}
+
+}}
---
+++ Boltz/Detail/SwapSetupHandler.hpp
@@ -0,0 +1,116 @@
+#ifndef BOLTZ_DETAIL_SWAPSETUPHANDLER_HPP
+#define BOLTZ_DETAIL_SWAPSETUPHANDLER_HPP
+
+#include"Ln/Amount.hpp"
+#include"Ln/Preimage.hpp"
+#include"Secp256k1/PrivKey.hpp"
+#include"Secp256k1/PubKey.hpp"
+#include"Ripemd160/Hash.hpp"
+#include"Sha256/Hash.hpp"
+#include"Sqlite3/Db.hpp"
+#include<cstdint>
+#include<memory>
+#include<string>
+#include<vector>
+
+namespace Boltz { class Connection; }
+namespace Boltz { class EnvIF; }
+namespace Ev { template<typename a> class Io; }
+namespace Secp256k1 { class SignerIF; }
+
+namespace Boltz { namespace Detail {
+
+/** class Boltz::Detail::SwapSetupHandler
+ *
+ * @brief sets up a reverse submarine swap with
+ * the given server, updating the database
+ * appropriately.
+ */
+class SwapSetupHandler
+	: public std::enable_shared_from_this<SwapSetupHandler> {
+private:
+	Secp256k1::SignerIF& signer;
+	Sqlite3::Db db;
+	Boltz::EnvIF& env;
+	std::string api_endpoint;
+	Boltz::Connection& conn;
+	Secp256k1::Random& random;
+	std::string destinationAddress;
+	Ln::Amount offchainAmount;
+
+	SwapSetupHandler( Secp256k1::SignerIF& signer_
+			, Sqlite3::Db db_
+			, Boltz::EnvIF& env_
+			, std::string const& api_endpoint_
+			, Boltz::Connection& conn_
+			, Secp256k1::Random& random_
+			, std::string const& destinationAddress_
+			, Ln::Amount offchainAmount_
+			) : signer(signer_)
+			  , db(std::move(db_))
+			  , env(env_)
+			  , api_endpoint(api_endpoint_)
+			  , conn(conn_)
+			  , random(random_)
+			  , destinationAddress(destinationAddress_)
+			  , offchainAmount(offchainAmount_)
+
+			  , tweak(random)
+			  , preimage(random)
+			  { }
+
+public:
+	static
+	std::shared_ptr<SwapSetupHandler>
+	create( Secp256k1::SignerIF& signer
+	      , Sqlite3::Db db
+	      , Boltz::EnvIF& env
+	      , std::string const& api_endpoint
+	      , Boltz::Connection& conn
+	      , Secp256k1::Random& random
+	      , std::string const& destinationAddress
+	      , Ln::Amount offchainAmount
+	      ) {
+		return std::shared_ptr<SwapSetupHandler>(
+			new SwapSetupHandler( signer
+					    , std::move(db)
+					    , env
+					    , api_endpoint
+					    , conn
+					    , random
+					    , destinationAddress
+					    , offchainAmount
+					    )
+		);
+	}
+
+	/* Returns the invoice.  */
+	Ev::Io<std::string> run();
+private:
+	Ev::Io<void> core_run();
+
+	Ev::Io<void> logd(std::string);
+	Ev::Io<void> loge(std::string);
+	std::string prefixlog(std::string);
+
+	/* Data we generate.  */
+	Secp256k1::PrivKey tweak;
+	Ln::Preimage preimage;
+	/* Derived from above.  */
+	Secp256k1::PubKey tweakPubKey;
+	Sha256::Hash preimageHash;
+	Ripemd160::Hash preimageHash160;
+
+	/* Data from server.  */
+	std::unique_ptr<std::string> swapId;
+	std::vector<std::uint8_t> redeemScript;
+	std::uint32_t timeoutBlockheight;
+	Ln::Amount onchainAmount;
+
+	/* The invoice to pay, or empty string on failure.  */
+	std::string invoice;
+};
+
+}}
+
+#endif /* !defined(BOLTZ_DETAIL_SWAPSETUPHANDLER_HPP) */

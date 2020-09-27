417591952f741f9d2a4ab98ea9b0a3451c7a4a37
ZmnSCPxj jxPCSnmZ <zmnscpxj@protonmail.com>
2020-09-27 15:55:58 +0800

    Boss/Mod/BoltzSwapper/Env.cpp: Created environment wrapper for BOSS.

---
+++ Boss/Mod/BoltzSwapper/Env.cpp
@@ -0,0 +1,76 @@
+#include"Bitcoin/Tx.hpp"
+#include"Boss/Mod/BoltzSwapper/Env.hpp"
+#include"Boss/Mod/Rpc.hpp"
+#include"Boss/Msg/Init.hpp"
+#include"Boss/log.hpp"
+#include"Ev/Io.hpp"
+#include"Ev/yield.hpp"
+#include"Jsmn/Object.hpp"
+#include"Json/Out.hpp"
+#include"S/Bus.hpp"
+
+namespace Boss { namespace Mod { namespace BoltzSwapper {
+
+void Env::start() {
+	bus.subscribe<Msg::Init
+		     >([this](Msg::Init const& init) {
+		rpc = &init.rpc;
+		return Ev::lift();
+	});
+}
+
+Ev::Io<std::uint32_t> Env::get_feerate() {
+	return Ev::yield().then([this]() {
+		/* Should not happen.  */
+		if (!rpc)
+			return Ev::lift<std::uint32_t>(2000);
+
+		return rpc->command("feerates"
+				   , Json::Out()
+					.start_object()
+						.field("style", "perkw")
+					.end_object()
+		).then([this](Jsmn::Object res) {
+			auto rate = res["perkw"]["opening"];
+			return Ev::lift<std::uint32_t>(double(rate));
+		});
+	});
+}
+
+Ev::Io<bool> Env::broadcast_tx(Bitcoin::Tx n_tx) {
+	auto tx = std::make_shared<Bitcoin::Tx>(std::move(n_tx));
+	return Ev::yield().then([this, tx]() {
+		/* Should not happen.  */
+		if (!rpc)
+			return Ev::lift(false);
+
+		return rpc->command( "sendrawtransaction"
+				   , Json::Out()
+					.start_object()
+						.field( "tx"
+						      , std::string(*tx)
+						      )
+						.field( "allowhighfees"
+						      , true
+						      )
+					.end_object()
+		).then([](Jsmn::Object res) {
+			return Ev::lift(true);
+		}).catching<RpcError>([](RpcError const& _) {
+			return Ev::lift(false);
+		});
+	});
+}
+
+Ev::Io<void> Env::logd(std::string msg) {
+	return Boss::log( bus, Debug
+			, "%s", msg.c_str()
+			);
+}
+Ev::Io<void> Env::loge(std::string msg) {
+	return Boss::log( bus, Warn
+			, "%s", msg.c_str()
+			);
+}
+
+}}}
---
+++ Boss/Mod/BoltzSwapper/Env.hpp
@@ -0,0 +1,42 @@
+#ifndef BOSS_MOD_BOLTZSWAPPER_ENV_HPP
+#define BOSS_MOD_BOLTZSWAPPER_ENV_HPP
+
+#include"Boltz/EnvIF.hpp"
+
+namespace Boss { namespace Mod { class Rpc; }}
+namespace S { class Bus; }
+
+namespace Boss { namespace Mod { namespace BoltzSwapper {
+
+/** class Boss::Mod::BoltzSwapper::Env
+ *
+ * @brief fulfills the `Boltz::EnvIF` interface,
+ * using the RPC and log of CLBOSS.
+ */
+class Env : public Boltz::EnvIF {
+private:
+	S::Bus& bus;
+	Boss::Mod::Rpc* rpc;
+
+	void start();
+
+public:
+	Env() =delete;
+	Env(Env&&) =delete;
+	Env(Env const&) =delete;
+
+	explicit
+	Env(S::Bus& bus_) : bus(bus_), rpc(nullptr) {
+		start();
+	}
+
+	/* Boltz::EnvIF.  */
+	Ev::Io<std::uint32_t> get_feerate() override;
+	Ev::Io<bool> broadcast_tx(Bitcoin::Tx) override;
+	Ev::Io<void> logd(std::string) override;
+	Ev::Io<void> loge(std::string) override;
+};
+
+}}}
+
+#endif /* !defined(BOSS_MOD_BOLTZSWAPPER_ENV_HPP) */

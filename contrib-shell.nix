9b69e8be1db1e38e95206b09b3868f90971b3528
Chris Guida <chrisguida@gmail.com>
2024-10-04 15:09:12 -0600

    Add contrib-shell.nix to allow running contrib scripts in nix
    Just do `nix-shell contrib-shell.nix` then run any of the python
    scripts in contrib/

---
+++ contrib-shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import (builtins.fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz";
+}) {} }:
+
+let
+  poetry2nix = import (builtins.fetchGit {
+    url = "https://github.com/nix-community/poetry2nix";
+  }) { inherit pkgs; };
+in
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.poetry
+    (poetry2nix.mkPoetryEnv {
+      projectDir = ./contrib;
+    })
+  ];
+}
+
